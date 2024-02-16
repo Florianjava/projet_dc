8fcef951dfa02729a34900c662cdb1bf65139697
Alvin Vedel <alvin.vedel@etu.univ-montp3.fr>
2024-02-16 17:18:16 +0100

    Modifs appel api twitch

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,8 @@ router.get('/', (req, res) => {
 
 const urlPost = 'https://id.twitch.tv/oauth2/token';
 
+const urlStream = "https://api.twitch.tv/helix/streams?user_login=";
+
 const params = new URLSearchParams();
 params.append('client_id', process.env.CLIENTID);
 params.append('client_secret', process.env.CLIENTSECRET);
@@ -21,10 +23,30 @@ const requestOptions = {
         'Content-Type': 'application/x-www-form-urlencoded',
     },
 };
+const requestOptions2 = {
+    method: 'POST',
+    body: params,
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    },
+};
 
 fetch(urlPost, requestOptions)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        //console.log(data);
+        let Bearer = data.token_type+" "+data.access_token;
+        console.log(Bearer);
+
+        const params2 = new URLSearchParams();
+        params2.append('client_id', process.env.CLIENTID);
+        params2.append('Authorization', Bearer);
+        params2.append('user_name', "gotaga");
+        fetch(urlStream, requestOptions2)
+            .then(response_user => response_user.json())
+            .then(data => console.log(data))
+
+    })
     .catch(error => console.error('Error:', error));
 
   req.log.info('/ request recieved')
