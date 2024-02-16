ce404f5383b0d7924f445a3a64f2f795dc08cdc1
Florian Dubois <duboisflorian07@gmail.com>
2024-02-16 16:44:58 +0100

    premier commit api twitch

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,33 @@
 import { Router } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const router = Router();
 
 router.get('/', (req, res) => {
-  req.log.info('/ request recieved');
 
-  res.send('Hello World!');
+const urlPost = 'https://id.twitch.tv/oauth2/token';
+
+const params = new URLSearchParams();
+params.append('client_id', process.env.CLIENTID);
+params.append('client_secret', process.env.CLIENTSECRET);
+params.append('redirect_uri', process.env.REDIRECTURI);
+params.append('grant_type', process.env.GRANTTYPE);
+
+const requestOptions = {
+    method: 'POST',
+    body: params,
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    },
+};
+
+fetch(urlPost, requestOptions)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+
+  req.log.info('/ request recieved')
 });
 
 export default router;
